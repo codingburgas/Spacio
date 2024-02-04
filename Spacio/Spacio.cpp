efe498fb12fd78f5b620ca1564b07917ba9ca051
Iva Raikova <IDRaykova22@codingburgas.bg>
2024-02-04 21:27:27 +0200

    Add answers in home menu. Close #8

--- Spacio/Spacio.cpp
+++ Spacio/Spacio.cpp
@@ -17,7 +17,10 @@ void homeMenu()
     Image image = LoadImage("../assets/images/mainCharacter.png");
     Texture2D mainCharacter = LoadTextureFromImage(image);
 
-    int questionCounter = 0;
+    Rectangle planetButton = { 648, 400, 150, 40 };
+    Rectangle higgsButton = { 648, 480, 180, 40 };
+    bool buttonClickedPlanets = false;
+    bool buttonClickedHiggs = false;
 
     const char* questions[8] = { "Do you choose to gaze at the stars\nor delve into the study\nof micro-particles?",
         "Do you prefer exploring galaxies\nor focusing on elementary particles?",
@@ -28,12 +31,31 @@ void homeMenu()
         "Would you enjoy astronomical\nobservation or analyzing data\nfrom particle experiments ?",
         "Do you prefer examining the vast\nstructures of the Universe or delving\ninto the basics of composite particles?" };
 
+    const char* planetsAnswers[8] = { "Stars", "Galaxies", "Cosmic phenomena", "Black holes",
+        "Stellar cores", "Gravitational waves", "Astronomical observation", "Vast structures" };
+
+    const char* higgsAnswers[8] = { "Micro-particles", "Elementary particles", "Atomic structures", "Subatomic interactions",
+        "Experimental results", "Interactions between quarks", "Analysis of particle data", "Composite particles" };
+
+    int questionCounter = 0;
+    int planets = 0;
+    int higgs = 0;
+
     SetTextLineSpacing(38);
 
     SetTargetFPS(60);
 
     while (!WindowShouldClose())
     {
+        if (CheckCollisionPointRec(GetMousePosition(), planetButton) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+        {
+            buttonClickedPlanets = true;
+        }
+
+        if (CheckCollisionPointRec(GetMousePosition(), higgsButton) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+        {
+            buttonClickedHiggs = true;
+        }
 
         BeginDrawing();
 
@@ -41,6 +63,26 @@ void homeMenu()
         DrawTexture(mainCharacter, -20, 80, WHITE);
 
         DrawTextEx(Poppins, questions[questionCounter], Vector2{ 600, 260 }, 35, 5, WHITE);
+
+        DrawRectangleRec(planetButton, GetColor(0x312b4700));
+        DrawTextEx(Poppins, planetsAnswers[questionCounter], Vector2{ 665, 400 }, 35, 5, WHITE);
+
+        if (buttonClickedPlanets)
+        {
+            planets++;
+            questionCounter++;
+            buttonClickedPlanets = false;
+        }
+
+        DrawRectangleRec(higgsButton, GetColor(0x312b4700));
+        DrawTextEx(Poppins, higgsAnswers[questionCounter], Vector2{ 665, 480 }, 35, 5, WHITE);
+
+        if (buttonClickedHiggs)
+        {
+            higgs++;
+            questionCounter++;
+            buttonClickedHiggs = false;
+        }
 
         EndDrawing();
     }
