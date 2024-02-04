620831e0979a99f355a18b9d5b7064d1f5dd81e4
Iva Raikova <IDRaykova22@codingburgas.bg>
2024-02-04 18:49:18 +0200

    Add questions, new libraries and namespace std. Close #4

--- Spacio/Spacio.cpp
+++ Spacio/Spacio.cpp
@@ -1,11 +1,26 @@
 #include "raylib.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
 
 void homeMenu()
 {
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenWidth = 1280;
+    const int screenHeight = 720;
 
     InitWindow(screenWidth, screenHeight, "Spacio");
+
+    int questionCounter = 0;
+
+    const char* questions[8] = { "Do you choose to gaze at the stars\nor delve into the study\nof micro-particles?",
+        "Do you prefer exploring galaxies\nor focusing on elementary particles?",
+        "Does your excitement come from\ncosmic phenomena or\natomic structures?",
+        "Would you rather investigate black\nholes or examine subatomic\ninteractions?",
+        "Do you ponder more on processes in\nstellar cores or on experimental\nresults in particles?",
+        "Are you into studying gravitational\nwaves or interactions between\nquarks?",
+        "Would you enjoy astronomical\nobservation or analyzing data\nfrom particle experiments ?",
+        "Do you prefer examining the vast\nstructures of the Universe or delving\ninto the basics of composite particles?" };
 
     SetTargetFPS(60);
 
@@ -16,7 +31,7 @@ void homeMenu()
 
         ClearBackground(RAYWHITE);
 
-        DrawText("Test", 190, 200, 20, LIGHTGRAY);
+        DrawTextEx(Poppins, questions[questionCounter], Vector2{ 600, 260 }, 35, 5, WHITE);
 
         EndDrawing();
     }
