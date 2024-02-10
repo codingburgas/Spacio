b497e6f7b4f929e2e4e278872c000c90ea8ad8d5
Iva Raikova <IDRaykova22@codingburgas.bg>
2024-02-10 15:25:11 +0200

    Add heading to characterPick.cpp and change its background

--- Spacio/src/characterPick.cpp
+++ Spacio/src/characterPick.cpp
@@ -4,7 +4,7 @@ void characterPick(Font Poppins, Font boldPoppins)
 {
     SetMouseCursor(MOUSE_CURSOR_ARROW);
 
-    Texture2D background = LoadTexture("../assets/images/chooseMenuBackground.png");
+    Texture2D background = LoadTexture("../assets/images/characterPickBackground.png");
 
     while (!WindowShouldClose())
     {
@@ -12,6 +12,9 @@ void characterPick(Font Poppins, Font boldPoppins)
         BeginDrawing();
 
         DrawTexture(background, 0, 0, RAYWHITE);
+
+        DrawTextEx(boldPoppins, "Choose a companion", Vector2(350, 60), 75, 5, WHITE);
+
         ClearBackground(RAYWHITE);
 
         EndDrawing();
