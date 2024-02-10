f2ba4cd614eea5829efb4c7301667103284524e2
Iva Raikova <IDRaykova22@codingburgas.bg>
2024-02-10 17:00:41 +0200

    Implement girl's astronaut. Close #20

--- Spacio/src/characterPick.cpp
+++ Spacio/src/characterPick.cpp
@@ -5,6 +5,8 @@ void characterPick(Font Poppins, Font boldPoppins)
     SetMouseCursor(MOUSE_CURSOR_ARROW);
 
     Texture2D background = LoadTexture("../assets/images/characterPickBackground.png");
+    
+    Texture2D girlAstrounat = LoadTexture("../assets/images/girlAstronaut.png");
 
     while (!WindowShouldClose())
     {
@@ -12,6 +14,7 @@ void characterPick(Font Poppins, Font boldPoppins)
         BeginDrawing();
 
         DrawTexture(background, 0, 0, RAYWHITE);
+        DrawTexture(girlAstrounat, 1018, 180, RAYWHITE);
 
         DrawTextEx(boldPoppins, "Choose a companion", Vector2(350, 60), 75, 5, WHITE);
 
