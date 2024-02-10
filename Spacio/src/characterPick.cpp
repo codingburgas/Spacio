d2848a24d6d92c456cb54acac0651ad0429b2b10
Iva Raikova <IDRaykova22@codingburgas.bg>
2024-02-10 12:12:08 +0200

    Fix cursor's bug in characterPick.cpp

--- Spacio/src/characterPick.cpp
+++ Spacio/src/characterPick.cpp
@@ -2,14 +2,14 @@
 
 void characterPick()
 {
+    SetMouseCursor(MOUSE_CURSOR_ARROW);
+
     while (!WindowShouldClose())
     {
 
         BeginDrawing();
 
         ClearBackground(RAYWHITE);
-
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
 
         EndDrawing();
     }
