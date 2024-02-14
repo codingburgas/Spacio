ec84c7690d6446f86e2e975f03778173308adee5
Iva Raikova <IDRaykova22@codingburgas.bg>
2024-02-14 17:18:55 +0200

    Implement userName.cpp. Close #31

--- Spacio/src/userName.cpp
+++ Spacio/src/userName.cpp
@@ -1,5 +1,15 @@
 #include "userName.h"
 
 void userName() {
+    Font Poppins = LoadFontEx("../assets/fonts/Poppins-Regular.ttf", 1000, NULL, 0);
+    Font boldPoppins = LoadFontEx("../assets/fonts/Poppins-Bold.ttf", 1000, NULL, 0);
+    Texture2D background = LoadTexture("../assets/images/getStationBackground.png");
 
+
+    Rectangle textBox = { 320, 420, 860, 120 };
+    const int maxChars = 15;
+    char name[maxChars + 1] = "\0";
+    int letterCount = 0;
+    bool mouseOnText = false;
+    int framesCounter = 0;
 }
