ad517ab132da48af6d9723ac9311a6cab8199ba2
Simeon Stefanov <SSStefanov22@codingburgas.bg>
2024-02-16 00:47:40 +0200

    Finish readyForQuiz file

--- Spacio/src/readyForQuiz.cpp
+++ Spacio/src/readyForQuiz.cpp
@@ -18,7 +18,9 @@ void Quiz()
     Font Poppins = LoadFontEx("../assets/fonts/Poppins-Regular.ttf", 1000, NULL, 0);
     Font boldPoppins = LoadFontEx("../assets/fonts/Poppins-Bold.ttf", 1000, NULL, 0);
 
-    Texture2D background = LoadTexture("../assets/images/readyForBackground.png");
+    Texture2D background = LoadTexture("../assets/images/readyBackground.png");
+    Texture2D starYes = LoadTexture("../assets/images/starYes.png");
+    Texture2D starNo = LoadTexture("../assets/images/starNo.png");
 
     Rectangle buttonYes = { 648, 500, 150, 40 };
     Rectangle ButtonNo = { 648, 580, 180, 40 };
@@ -33,8 +35,10 @@ void Quiz()
         ClearBackground(RAYWHITE);
 
         DrawTexture(background, 0, 0, RAYWHITE);
+        DrawTexture(starYes, 800, 150, RAYWHITE);
+        DrawTexture(starNo, 800, 450, RAYWHITE);
 
-        DrawTextEx(boldPoppins, "Are you ready for a quiz", Vector2(500, 50), 50, 5, LIGHTGRAY);
+        DrawTextEx(boldPoppins, "Are you ready for a quiz", Vector2(450, 50), 50, 5, WHITE);
 
         EndDrawing();
     }
