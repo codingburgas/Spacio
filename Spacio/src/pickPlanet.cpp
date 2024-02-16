4662c9a965e224439e3fc56fe803a18449a6631c
Iva Raikova <IDRaykova22@codingburgas.bg>
2024-02-16 15:55:46 +0200

    Add pickPlanet's background. Close #40

--- Spacio/src/pickPlanet.cpp
+++ Spacio/src/pickPlanet.cpp
@@ -5,7 +5,7 @@ void pickPlanet()
     Font Poppins = LoadFontEx("../assets/fonts/Poppins-Regular.ttf", 1000, NULL, 0);
     Font boldPoppins = LoadFontEx("../assets/fonts/Poppins-Bold.ttf", 1000, NULL, 0);
 
-    Texture2D background = LoadImage();
+    Texture2D background = LoadTexture("../assets/images/pickPlanetBackground.png");
     while (!WindowShouldClose())
     {
 
