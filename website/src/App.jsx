97fc09041f62201ecf4ab62f1d6595447ee34185
Iva Raikova <IDRaykova22@codingburgas.bg>
2024-02-20 23:48:46 +0200

    Add astronaut image for website and some paragraphs. Close #51

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -3,30 +3,75 @@ import './App.css';
 import logo from './assets/logo.png';
 import arrowDown from './assets/arrow.png';
 import smallCircleTopLeft from './assets/smallGradientCircleTopLeft.png';
-import gradientCircleTopLeft from './assets/gradientCircleTopLeft.png'
+import gradientCircleTopLeft from './assets/gradientCircleTopLeft.png';
+import middleRightCircleGradient from './assets/middleRightCircleGradient.png';
+import astronaut from './assets/astronaut.png';
 
 function App() {
   return (
-  <div className='bg-[#080310] min-h-screen w-screen overflow-x-hidden'>
-    <div>
-      <div className='overflow-x-hidden'>
-          <img src={smallCircleTopLeft} alt="Background gradient" className='top-0 left-0 absolute select-none'/>
-          <img src={gradientCircleTopLeft} alt="Background gradient" className='top-0 absolute select-none'/>
-      </div>
-
-      <div className="flex items-center text-white cursor-pointer">
-        <img src={logo} alt="Spacio's logo" className="w-16 h-auto select-none z-0 ml-1" />
-        <h1 className="ml-3 font-bold z-0 text-2xl">Spacio</h1>
-      </div>
-        
-      <div className='text-center my-40 mt-60 text-white'>
-        <h1 className='text-9xl font-bold z-0 relative'>SPACIO</h1>
-        <div className="flex justify-center">
-          <img src={arrowDown} alt="Arrow down" className='select-none cursor-pointer object-center mx-auto mt-20 scale-75'/>
+    <div className="bg-[#080310] min-h-screen w-screen overflow-x-hidden">
+      <div>
+        <div className="overflow-x-hidden">
+          <img
+            src={smallCircleTopLeft}
+            alt="Background gradient"
+            className="top-0 left-0 absolute select-none"
+          />
+          <img
+            src={gradientCircleTopLeft}
+            alt="Background gradient"
+            className="top-0 absolute select-none"
+          />
         </div>
+
+        <div className="flex items-center text-white cursor-pointer">
+          <img
+            src={logo}
+            alt="Spacio's logo"
+            className="w-16 h-auto select-none z-0 ml-1"
+          />
+          <h1 className="ml-3 font-bold z-0 text-2xl">Spacio</h1>
+        </div>
+
+        <div className="text-center my-40 mt-60 text-white">
+          <h1 className="text-9xl font-bold z-0 relative">SPACIO</h1>
+          <div className="flex justify-center">
+            <img
+              src={arrowDown}
+              alt="Arrow down"
+              className="select-none cursor-pointer object-center mx-auto mt-20 scale-75"
+            />
+          </div>
+        </div>
+
+        <div className="flex text-white mt-96">
+          <img
+            src={middleRightCircleGradient}
+            alt="Background gradient"
+            className="right-0 absolute select-none"
+          />
+
+          <div className="ml-48 z-0">
+            <h1 className="text-6xl font-extralight z-0 mb-20">
+              About{" "}
+              <span className="font-semibold" style={{ color: "#81689D" }}>
+                Spacio
+              </span>
+            </h1>
+
+            <p className="text-5xl font-light z-0 max-w-2xl">
+              A one-of-a-kind educational application that offers endless excitement
+              and knowledge for your brain. Download it and challenge yourself to learn
+              as much as possible!
+            </p>
+        </div>
+
+        <img src={astronaut} alt="Astronaut's image" className="w-6/12 h-2/4 select-none" />
+        </div>
+
+
       </div>
     </div>
-  </div>
   );
 }
 
