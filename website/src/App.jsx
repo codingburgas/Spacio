5d20822fd20c551060f5916ddb216cd93a96c326
Iva Raikova <IDRaykova22@codingburgas.bg>
2024-02-20 22:18:20 +0200

    Implement landing page in website #50

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,32 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/logo.png'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import logo from './assets/logo.png';
+import arrowDown from './assets/arrow.png';
+import smallCircleTopLeft from './assets/smallGradientCircleTopLeft.png';
+import gradientCircleTopLeft from './assets/gradientCircleTopLeft.png'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  <div className='bg-[#080310] min-h-screen w-screen overflow-x-hidden'>
+    <div>
+      <div className='overflow-x-hidden'>
+          <img src={smallCircleTopLeft} alt="Background gradient" className='top-0 left-0 absolute select-none'/>
+          <img src={gradientCircleTopLeft} alt="Background gradient" className='top-0 absolute select-none'/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+
+      <div className="flex items-center text-white">
+        <img src={logo} alt="Spacio's logo" className="w-16 h-auto select-none z-0" />
+        <h2 className="ml-2 font-bold z-0">Spacio</h2>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+        
+      <div className='text-center my-40 mt-60 text-white'>
+        <h1 className='text-9xl font-bold z-0 relative'>SPACIO</h1>
+        <div className="flex justify-center">
+          <img src={arrowDown} alt="Arrow down" className='select-none cursor-pointer object-center mx-auto mt-20 scale-75'/>
+        </div>
+      </div>
+    </div>
+  </div>
+  );
 }
 
-export default App
+export default App;
